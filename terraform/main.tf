d87a1e0d5d514f7f263a925f1df1f8a4e8445c24
Andrew Zick <aezick@umich.edu>
2020-05-25 23:34:21 -0700

    Wild and free, please dont hack my box

--- terraform/main.tf
+++ terraform/main.tf
@@ -332,7 +332,7 @@ resource "aws_security_group" "personal-website-sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [aws_vpc.personal-website-vpc.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
