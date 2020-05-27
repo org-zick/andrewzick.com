80a0aae2c60dfeccd027b991b8a212f8988fc4f5
Andrew Zick <aezick@umich.edu>
2020-05-26 22:55:47 -0700

    Add key pair for sshing into EC2 instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,11 @@ resource "aws_kms_key" "s3-enc-key" {
   enable_key_rotation     = true
 }
 
+resource "aws_key_pair" "aws-ec2-ssh-key-pair" {
+  key_name   = "aws-ec2-ssh-key-pair"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpUslKoi+m+7Vj1ks+vc83KaQkb7XQc2i+OVC7S0ZpyWs0B201p+GbTenRbqjmkY5fGaZ/4XRiwXHwtn17D3wLaf5LtibL0kTKDJLrFEvdof0Q/TMPRaSE62yeY4zrJ1X6iUqEPe6gPql1jHjco0Yjz1iM0suok1IMLV/OlSWiCSA52HMYikvCRuq3P7XPPv/WKbpBZfXHT37z9atwcTjTAKueIRTBthQdMU0Ntetas0h48XXxBEJHxB0niq9cAIvcffTDJbmRoDnLPFySsa/RNLgmYlEwjxqwfzSA+yTwErF1Vl3OUw/0YF7TXcI7u7FQH/8NIeKmnuMLaNm112v5 andrewzick@gmail.com"
+}
+
 resource "aws_s3_bucket" "tf-state-s3" {
   bucket = "personal-website-tf-state-${var.environment}"
   region = var.region
