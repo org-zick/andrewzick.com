b9843cc312a5cbb7e474dc5ba93b966116c1d530
Andrew Zick <aezick@umich.edu>
2020-06-17 19:24:48 -0700

    Attempt using an ELB for something?

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -23,6 +23,35 @@ resource "aws_security_group" "pw-sg-allow-ssh" {
   }
 }
 
+resource "aws_security_group" "pw-sg-elb-web-traffic" {
+  name   = "elb_web_traffic"
+  vpc_id = aws_vpc.pw-vpc.id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+}
+
 resource "aws_subnet" "pw-public-subnet" {
   vpc_id            = aws_vpc.pw-vpc.id
   cidr_block        = "10.0.0.0/26"
@@ -45,4 +74,25 @@ resource "aws_route_table" "pw-route-table" {
 resource "aws_route_table_association" "pw-public-route-table-assoc" {
   subnet_id      = aws_subnet.pw-public-subnet.id
   route_table_id = aws_route_table.pw-route-table.id
+}
+
+resource "aws_elb" "pw-elb" {
+  name               = "pw-elb"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  security_groups    = [aws_security_group.pw-sg-elb-web-traffic.id]
+  instances          = [aws_instance.caddy-test-ec2-instance.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  listener {
+    instance_port     = 443
+    instance_protocol = "https"
+    lb_port           = 443
+    lb_protocol       = "https"
+  }
 }
