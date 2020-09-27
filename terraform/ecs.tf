2799847a57fd4e167ce49277e25de055b53ed83c
Andrew Zick <aezick@umich.edu>
2020-09-27 16:57:12 -0700

    ECS cluster, service, task, all that stuff

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,115 +1,115 @@
-# resource "aws_cloudwatch_log_group" "personal-website-logs" {
-#   name              = "personal-website-logs"
-#   retention_in_days = 7
-# }
+resource "aws_cloudwatch_log_group" "personal-website-logs" {
+  name              = "personal-website-logs"
+  retention_in_days = 7
+}
 
-# resource "aws_ecs_cluster" "personal-website-cluster" {
-#   name = local.cluster_name
-#   capacity_providers = [aws_ecs_capacity_provider.personal-website-cp.name]
+resource "aws_ecs_cluster" "personal-website-cluster" {
+  name = local.cluster_name
+  capacity_providers = [aws_ecs_capacity_provider.personal-website-cap-provider.name]
 
-#   default_capacity_provider_strategy {
-#     capacity_provider = aws_ecs_capacity_provider.personal-website-cp.name
-#   }
-# }
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.personal-website-cap-provider.name
+  }
+}
 
-# resource "aws_iam_role" "personal-website-task-role" {
-#   name = "personal-website-task-role"
+resource "aws_iam_role" "personal-website-task-role" {
+  name = "personal-website-task-role"
 
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Sid": "",
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "ecs-tasks.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-# }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_policy" "personal-website-task-policy" {
-#   name  = "personal-website-task-policy"
+resource "aws_iam_policy" "personal-website-task-policy" {
+  name  = "personal-website-task-policy"
 
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Action": [
-#         "ecr:GetAuthorizationToken",
-#         "ecr:BatchCheckLayerAvailability",
-#         "ecr:GetDownloadUrlForLayer",
-#         "ecr:BatchGetImage",
-#         "logs:CreateLogStream",
-#         "logs:PutLogEvents"
-#       ],
-#       "Resource": "*"
-#     }
-#   ]
-# }
-# EOF
-# }
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_role_policy_attachment" "personal-website-task-policy-attachment" {
-#   role       = aws_iam_role.personal-website-task-role.name
-#   policy_arn = aws_iam_policy.personal-website-task-policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "personal-website-task-policy-attachment" {
+  role       = aws_iam_role.personal-website-task-role.name
+  policy_arn = aws_iam_policy.personal-website-task-policy.arn
+}
 
-# resource "aws_ecs_task_definition" "personal-website-task-definition" {
-#   family                = "personal-website-task"
-#   task_role_arn         = aws_iam_role.personal-website-task-role.arn
-#   execution_role_arn    = aws_iam_role.personal-website-task-role.arn
-#   container_definitions = <<EOF
-# [
-#   {
-#     "name": "personal-website",
-#     "image": "153765495495.dkr.ecr.us-east-1.amazonaws.com/personal-website:v1",
-#     "cpu": 128,
-#     "memoryReservation": 128,
-#     "essential": true,
-#     "portMappings": [
-#       {
-#         "containerPort": 80,
-#         "hostPort": 80
-#       },
-#       {
-#         "containerPort": 443,
-#         "hostPort": 443
-#       }
-#     ],
-#     "mountPoints": [
-#       {
-#         "sourceVolume": "personal-website-caddy-data",
-#         "containerPath": "/data"
-#       }
-#     ],
-#     "logConfiguration": {
-#       "logDriver": "awslogs",
-#       "options": {
-#         "awslogs-region": "us-east-1",
-#         "awslogs-group": "personal-website-logs",
-#         "awslogs-stream-prefix": "complete-ecs"
-#       }
-#     }
-#   }
-# ]
-# EOF
+resource "aws_ecs_task_definition" "personal-website-task-definition" {
+  family                = "personal-website-task"
+  task_role_arn         = aws_iam_role.personal-website-task-role.arn
+  execution_role_arn    = aws_iam_role.personal-website-task-role.arn
+  container_definitions = <<EOF
+[
+  {
+    "name": "personal-website",
+    "image": "153765495495.dkr.ecr.us-east-1.amazonaws.com/personal-website:v1",
+    "cpu": 128,
+    "memoryReservation": 128,
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      },
+      {
+        "containerPort": 443,
+        "hostPort": 443
+      }
+    ],
+    "mountPoints": [
+      {
+        "sourceVolume": "personal-website-caddy-data",
+        "containerPath": "/data"
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-region": "us-east-1",
+        "awslogs-group": "personal-website-logs",
+        "awslogs-stream-prefix": "complete-ecs"
+      }
+    }
+  }
+]
+EOF
 
-#   volume {
-#     name = "personal-website-caddy-data"
+  volume {
+    name = "personal-website-caddy-data"
 
-#     docker_volume_configuration {
-#       scope         = "shared"
-#       autoprovision = true
-#     }
-#   }
-# }
+    docker_volume_configuration {
+      scope         = "shared"
+      autoprovision = true
+    }
+  }
+}
 
 resource "aws_iam_role" "container-instance-ec2-role" {
   name               = "container-instance-ec2-role"
@@ -220,39 +220,51 @@ resource "aws_autoscaling_group" "personal-website-asg" {
   }
 }
 
-# resource "aws_ecs_capacity_provider" "personal-website-cp" {
-#   name = "personal-website-cp"
+resource "aws_ecs_capacity_provider" "personal-website-cap-provider" {
+  name = "personal-website-cap-provider"
 
-#   auto_scaling_group_provider {
-#     auto_scaling_group_arn = aws_autoscaling_group.personal-website-asg.arn
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.personal-website-asg.arn
 
-#     managed_scaling {
-#       maximum_scaling_step_size = 10
-#       minimum_scaling_step_size = 1
-#       status                    = "ENABLED"
-#       target_capacity           = 75
-#     }
-#   }
-# }
+    managed_scaling {
+      maximum_scaling_step_size = 10
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 75
+    }
+  }
+}
 
-# resource "aws_ecs_service" "personal-website-service" {
-#   name            = "personal-website-service"
-#   cluster         = aws_ecs_cluster.personal-website-cluster.id
-#   task_definition = aws_ecs_task_definition.personal-website-task-definition.arn
-#   desired_count   = 1
+resource "aws_ecs_service" "personal-website-service" {
+  name            = "personal-website-service"
+  cluster         = aws_ecs_cluster.personal-website-cluster.id
+  task_definition = aws_ecs_task_definition.personal-website-task-definition.arn
+  desired_count   = 1
 
-#   capacity_provider_strategy {
-#     capacity_provider = aws_ecs_capacity_provider.personal-website-cp.arn
-#     weight            = 100
-#   }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.pw-nlb-target-group-port-80.arn
+    container_name   = "personal-website"
+    container_port   = 80
+  }
 
-#   ordered_placement_strategy {
-#     type  = "binpack"
-#     field = "cpu"
-#   }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.pw-nlb-target-group-port-443.arn
+    container_name   = "personal-website"
+    container_port   = 443
+  }
 
-#   # Optional: Allow external changes without Terraform plan difference
-#   lifecycle {
-#     ignore_changes = [desired_count]
-#   }
-# }
+  capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.personal-website-cap-provider.arn
+    weight            = 100
+  }
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  # Optional: Allow external changes without Terraform plan difference
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
+}
