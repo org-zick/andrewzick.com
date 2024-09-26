ace9ecf2f678b6c9af48513ebb370fa7049bc3f3
Andrew Zick <andrewzick@gmail.com>
2024-09-25 22:01:52 -0700

    DNS via Cloudflare

--- terraform/main.tf
+++ terraform/main.tf
@@ -199,3 +199,23 @@ data "cloudflare_zones" "domain" {
     name = var.site_domain
   }
 }
+
+resource "cloudflare_record" "site_cname" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = var.site_domain
+  value   = aws_s3_bucket_website_configuration.site.website_endpoint
+  type    = "CNAME"
+
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "www" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "www"
+  value   = var.site_domain
+  type    = "CNAME"
+
+  ttl     = 1
+  proxied = true
+}
